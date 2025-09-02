84af1f17d13965f806a7198538fdb2897d28e43f
biruk1122 <birukfikadu29@gmail.com>
2025-09-02 15:54:09 +0300

    adding the search feature

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -6,10 +6,11 @@ import humidityIcon from "../assets/humidity-icon.png"
 import rainIcon from "../assets/rain-icon.png"
 import snowhIcon from "../assets/snow-icon.png"
 import windIcon from "../assets/wind-icon.png"
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 function Weather() {
   const [weatherData, setWeatherData] = useState({})
+  const inputRef = useRef()
 
   const allIcons = {
     "01d": clearIcon,
@@ -29,6 +30,10 @@ function Weather() {
   }
 
   const search = async (city) => {
+    if (city === "") {
+      alert("First Enter Country Cities")
+      return
+    }
     try {
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${
         import.meta.env.VITE_WEATHER_API_ID
@@ -36,6 +41,12 @@ function Weather() {
 
       const response = await fetch(url)
       const data = await response.json()
+
+      if (!response.ok) {
+        alert(data.message)
+        return
+      }
+
       console.log(data)
       const icon = allIcons[data.weather[0].icon] || clearIcon
       setWeatherData({
@@ -45,11 +56,14 @@ function Weather() {
         location: data.name,
         icon: icon,
       })
-    } catch (error) {}
+    } catch (error) {
+      setWeatherData(false)
+      console.error("Error in Fetching weather Data")
+    }
   }
 
   useEffect(() => {
-    search("new york")
+    search("estanbul")
   }, [])
 
   return (
@@ -59,6 +73,7 @@ function Weather() {
         <div className="flex items-center gap-3 mb-8">
           <input
             className="flex-1 h-12 rounded-full pl-5 pr-4 text-gray-700 text-lg bg-[#ebfffc] outline-none placeholder:text-gray-400"
+            ref={inputRef}
             type="text"
             placeholder="Search city..."
           />
@@ -66,46 +81,55 @@ function Weather() {
             className="h-12 w-12 p-3 rounded-full bg-[#ebfffc] cursor-pointer hover:scale-105 transition"
             src={searchIcon}
             alt="Search Icon"
+            onClick={() => search(inputRef.current.value)}
           />
         </div>
 
-        {/* Weather Info */}
-        <div className="flex flex-col items-center text-center mb-10">
-          <img
-            className="h-40 w-40 mb-6"
-            src={weatherData.icon}
-            alt="Weather Icon"
-          />
-          <p className="text-white text-7xl font-bold leading-none mb-2">
-            {weatherData.temprature}&#176;C
-          </p>
-          <p className="text-white text-3xl font-medium">
-            {weatherData.location}
-          </p>
-        </div>
-
-        {/* Weather Details */}
-        <div className="flex justify-between text-white">
-          {/* Humidity */}
-          <div className="flex items-center gap-3">
-            <img className="h-10 w-10" src={humidityIcon} alt="Humidity" />
-            <div>
-              <p className="text-lg font-semibold">{weatherData.humidity}%</p>
-              <span className="text-sm text-gray-200">Humidity</span>
-            </div>
-          </div>
-
-          {/* Wind Speed */}
-          <div className="flex items-center gap-3">
-            <img className="h-10 w-10" src={windIcon} alt="Wind" />
-            <div>
-              <p className="text-lg font-semibold">
-                {weatherData.windspeed} km/h
+        {weatherData ? (
+          <>
+            {/* Weather Info */}
+            <div className="flex flex-col items-center text-center mb-10">
+              <img
+                className="h-40 w-40 mb-6"
+                src={weatherData.icon}
+                alt="Weather Icon"
+              />
+              <p className="text-white text-7xl font-bold leading-none mb-2">
+                {weatherData.temprature}&#176;C
               </p>
-              <span className="text-sm text-gray-200">Wind Speed</span>
+              <p className="text-white text-3xl font-medium">
+                {weatherData.location}
+              </p>
             </div>
-          </div>
-        </div>
+
+            {/* Weather Details */}
+            <div className="flex justify-between text-white">
+              {/* Humidity */}
+              <div className="flex items-center gap-3">
+                <img className="h-10 w-10" src={humidityIcon} alt="Humidity" />
+                <div>
+                  <p className="text-lg font-semibold">
+                    {weatherData.humidity}%
+                  </p>
+                  <span className="text-sm text-gray-200">Humidity</span>
+                </div>
+              </div>
+
+              {/* Wind Speed */}
+              <div className="flex items-center gap-3">
+                <img className="h-10 w-10" src={windIcon} alt="Wind" />
+                <div>
+                  <p className="text-lg font-semibold">
+                    {weatherData.windspeed} km/h
+                  </p>
+                  <span className="text-sm text-gray-200">Wind Speed</span>
+                </div>
+              </div>
+            </div>
+          </>
+        ) : (
+          <></>
+        )}
       </div>
     </div>
   )
